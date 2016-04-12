016510d53ce7b9fdc47a55c2963f7b4fb8e06b88
fabioM77 <cfabiomoreno@gmail.com>
2016-04-12 13:04:42 +0200

    Added files via upload

---
+++ programa.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+public class programa {
+
+	//rellenar matriz y mostrarlo.
+	public static void main(String[] args) {
+		
+		Scanner nc = new Scanner(System.in);
+	
+		int [] [] matriz = new int [2] [2];
+		
+		//rellenamos la matriz.
+		
+		for (int i=0; i<matriz.length;i++ ){
+			
+			for(int j=0; j<matriz[0].length; j++){
+			
+				System.out.print("dame la posicion matriz["+i+"]["+j+"]:");
+				matriz[i][j]= nc.nextInt();
+			}
+		}
+		
+		//mostrmaos las posiciones de la matriz.
+		for (int i=0; i<matriz.length;i++ ){
+			
+			for(int j=0; j<matriz[0].length; j++){
+				
+				System.out.println("en la posicion matriz["+i+"]["+j+"] hay un:" +matriz[i][j]);	
+			}
+		}
+			
+
+	}
+
+}
